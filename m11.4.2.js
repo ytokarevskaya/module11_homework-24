3ea3d5766cdbcf0e942a1b859efc8ce67c79ad31
ytokarevskaya <lan2214@yandex.ru>
2020-11-01 15:16:43 +0300

    Проверено

--- m11.4.2.js
+++ m11.4.2.js
@@ -3,14 +3,11 @@ function getNum(numm){
     switch (numm) {
       case 0:
         return console.log(numm1 + '- число равно 0');
-        break;
       case 1:
         return console.log(numm1 + '- число равно 1 - это простое число');
-        break;
       default:
         if (numm > 1000) {
           return console.log(numm1 + '- число более 1000')
-          break
         } else {
         for (let i = 2; i < numm; i++) {
          if (numm % i == 0) {
@@ -18,10 +15,14 @@ function getNum(numm){
           break
          }}}
     }
-      if ((a == true) && (numm != 0) && (numm != 1)) {
-              return console.log(numm1 + '- это простое число')
-      } else if (a == false) {
+      if (a) {
+          return console.log(numm1 + '- это простое число')
+      } else {
           return console.log(numm1 + '- это не простое число')}
     }
   let numm1 = 0;
 getNum(numm1);
+
+// Задание выполнено верно, но есть несколько замечаний по форме:
+// 1. В случае с 0, 1 и значениями больше 1000 вы используете директиву return и функция заканчивает выполнение, поэтому нет смысла в последующем условном операторе делать доп. проверки на значения 0 и 1. По той же причине нет смысла писать после return директиву break - выполнение функции, а значит и цикла, и так закончится на return.
+// 2. Переменная а - это логическое значение и писать if(a == true) - избыточно. Достаточно будет просто if(a)
